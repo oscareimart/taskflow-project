c970f9406bf19a1cd54b1db921d101439325a40a
Oscar Eimar <oeticona@gmail.com>
2026-03-23 21:08:28 -0400

    variables de entorno

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.URL_BACKEND || "http://localhost:5000/api",
 });
 
 export default API;
