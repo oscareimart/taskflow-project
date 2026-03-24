5195e201392cb6a2c6d85b647a8e82165f152866
Oscar Eimar <oeticona@gmail.com>
2026-03-23 21:35:06 -0400

    variables de entorno3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// console.log(process.env.REACT_APP_URL_BACKEND);
+
 const API = axios.create({
-  baseURL: process.env.URL_BACKEND, // || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_URL_BACKEND, // || "http://localhost:5000/api",
 });
 
 export default API;
