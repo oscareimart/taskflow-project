b533621915b533a23c7bfd4872f36da11965dbbb
Oscar Eimar <oeticona@gmail.com>
2026-03-30 20:56:01 -0400

    pruebas lazy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+// import React, { Suspense, lazy } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
+// const Dashboard = lazy(() => import("./pages/Dashboard"));
+
 function App() {
   return (
     <div>
-      <img src="/image.png" alt="Imagen prueba" />
       <Login />
       <Register />
+      {/* <Suspense fallback={<h2> Cargando... </h2>}> */}
       <Dashboard />
+      {/* </Suspense> */}
     </div>
   );
 }
