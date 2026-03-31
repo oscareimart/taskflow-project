86e1dbc6084247b6dbf598cb289d65201214c8a0
Oscar Eimar <oeticona@gmail.com>
2026-03-30 21:12:33 -0400

    prueba rendimiento cache

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,6 +4,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_URL_BACKEND, // || "http://localhost:5000/api",
+  headers: {
+    "Cache-Control": "public, max-age=31536000",
+  },
 });
 
 export default API;
