4419462824a047d6bee523f9d79d637d1234db3d
Oscar Eimar <oeticona@gmail.com>
2026-03-30 21:19:45 -0400

    cambios cache frontend y backend

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
+app.use(
+  express.static("public", {
+    maxAge: "1y",
+  }),
+);
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/authRoutes"))
-app.use("/api/tasks", require("./routes/taskRoutes"))
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
 
-mongoose.connect(process.env.MONGO_URI
-).then(() => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("Conectado a MongoDB");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.error("Error al conectar a MongoDB:", err);
-});
+  });
 
 app.get("/", (req, res) => {
-    res.send("API Funcionando!");
+  res.send("API Funcionando!");
 });
 
 app.listen(5000, () => {
-    console.log("Servidor corriendo en puerto 5000");
+  console.log("Servidor corriendo en puerto 5000");
 });
