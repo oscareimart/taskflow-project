a0e3e90a6c7ec0226470e2dfa0c6a63ade0ec0fd
Oscar Eimar <oeticona@gmail.com>
2026-03-30 20:58:27 -0400

    pruebas lazy 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-// import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
-// const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 function App() {
   return (
     <div>
       <Login />
       <Register />
-      {/* <Suspense fallback={<h2> Cargando... </h2>}> */}
-      <Dashboard />
-      {/* </Suspense> */}
+      <Suspense fallback={<h2> Cargando... </h2>}>
+        <Dashboard />
+      </Suspense>
     </div>
   );
 }
